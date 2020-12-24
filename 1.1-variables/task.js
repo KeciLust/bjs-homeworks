79c53b983260660b99fc98f8780fae29fb71c75c
Denis Nikitin <dinya.nikitin@gmail.com>
2020-12-24 21:08:18 +0300

    3

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -1,25 +1,24 @@
-
 function averageMark(a, g, p) {
 	let algebra = a;
 	let geography = g;
 	let physics = p;
-    
-    
+
+
 	return (a + g + p) / 3;
 }
 
 function sayHello(userName) {
-	
-    let myName = userName;
-    
+
+	let myName = userName;
+
 	return `Привет, мир! Меня зовут ${myName}`;
 }
 
 function calculateFormula() {
-	
+
 	let x = 2;
 	let y = 22;
 	let z = 0;
-    let result = x * y + 5 * z + x - 1;
-    return result;
+	let result = x * y + 5 * z + x - 1;
+	return result;
 }
