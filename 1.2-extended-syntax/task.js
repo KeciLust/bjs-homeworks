248f81ad92c06e3fd1c22414be19b2ea21154447
Denis Nikitin <dinya.nikitin@gmail.com>
2021-01-06 16:28:01 +0300

    1.2 first change

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -3,8 +3,8 @@ function getResult(a,b,c){
     let d = b** - 4 * a * c;
     let x = [];
     if ( d > 0) {
-        x[0] = ( -b - Math.SQRT(b** - 4 * a * c)) / 2 * a;
-        x[1] = ( -b + Math.SQRT(b** - 4 * a * c)) / 2 * a;
+        x[0] = ( -b - Math.sqrt(b** - 4 * a * c)) / 2 * a;
+        x[1] = ( -b + Math.sqrt(b** - 4 * a * c)) / 2 * a;
     } else if ( d === 0) {
         x[0] = ( -b)/ 2 * a;
     }     
