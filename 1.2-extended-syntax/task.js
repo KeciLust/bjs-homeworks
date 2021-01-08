29ebd0fd290f030af27a29e77141900ef4502ee5
Denis Nikitin <dinya.nikitin@gmail.com>
2021-01-08 10:38:31 +0300

    1.2 Finish change 2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -4,8 +4,8 @@ function getResult(a, b, c) {
     let d = Math.pow(b, 2) - 4 * a * c;
     let x = [];
     if (d > 0) {
-        x.push((-b - Math.sqrt(d)) / 2 * a);
         x.push((-b + Math.sqrt(d)) / 2 * a);
+        x.push((-b - Math.sqrt(d)) / 2 * a);
     } else if (d === 0) {
         x.push(-b / (2 * a));
     }
@@ -16,7 +16,7 @@ function getAverageMark(marks) {
     let quantityMarks;
     let sumMarks = 0;
     if (marks.length == 0) {
-        return "0";
+        return 0;
     }
     quantityMarks = marks.slice(0, 5);
     for (let i = 0; i < quantityMarks.length; i++) {
@@ -26,14 +26,11 @@ function getAverageMark(marks) {
 }
 
 function askDrink(name, dateOfBirthday) {
-    let fullTime = dateOfBirthday.getTime();
     let now = new Date();
-    let result
-    let checkAge = now - fullTime - 568036800000 //Столько миллисекунд в 18 годах(к каждому году добавленно 6 часов)
-    if (checkAge >= 0) {
-        result = `Не желаете ли олд-фэшн, ${name}?`;
-    } else {
-        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`
+    let checkAge = now.getFullYear() - dateOfBirthday.getFullYear();
+    if (checkAge >= 18) {
+        return `Не желаете ли олд-фэшн, ${name}?`;
     }
-    return result;
+    return `Сожалею, ${name}, но я не могу вам продать алкоголь. 
+    Могу предложить вам замечательный клюквенный компот!`;
 }
