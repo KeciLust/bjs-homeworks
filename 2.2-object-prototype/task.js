ee62f43c1a893539cfaf3d2ffcb58bcee941fc29
Denis Nikitin <dinya.nikitin@gmail.com>
2021-01-12 21:32:32 +0300

    2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,44 @@
+"use strict";
 //String.prototype.isPalindrome - для задачи №1
+String.prototype = isPalindrome();
+
+function isPalindrome(string) {
+    let backString = "";
+    string = string.toUpperCase();
+    for (let i = string.length; i > 0; i--) {
+        backString += string[i];
+    }
+    if (string === backString) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let mark = 0;
+    if (marks.length === 0) {
+        return averageMark = 0;
+    } else {
+        for (let i = 0; i < marks.length; i++) {
+            mark += marks[i];
+        }
+        let average = mark / marks.length;
+        let averageMark = Math.round(average);
+
+        return averageMark
+    }
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Date.now();
+    birthday = new Date(birthday);
+    let diff = now - birthday.getTime();
+    let age = diff / (((365 * 24) + 6) * 60 * 60 * 1000)
+    let verdict;
+if ( age >= 18) {
+    return true;
+}
+    return false;
 }
