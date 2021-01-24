83d33689a3aef0945025b9a367a8c8d0e389c71a
Denis Nikitin <dinya.nikitin@gmail.com>
2021-01-24 17:06:00 +0300

    3.1

---
+++ 3.1-exceptions-closure/task.js
@@ -0,0 +1,51 @@
+function parseCount(number) {
+    let newNumber = Number.parseInt(number);
+    if (isNaN(newNumber)) {
+        const e = new Error("Невалидное значение");
+        throw e;
+    }
+    return newNumber;
+}
+
+function validateCount(number) {
+    try {
+        return parseCount(number);
+    } catch (e) {
+        return e;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if ((a + b) < c || (a + c) < b || (c + b) < a) {
+            const e = new Error("Треугольник с такими сторонами не существует");
+            throw e;
+        }
+    }
+    getPerimeter() {
+        return this.a + this.b + this.c;;
+    }
+    getArea() {
+        const p = this.getPerimeter();
+        return Math.sqrt((p / 2) * (p / 2 - this.a) * (p / 2 - this.b) * (p / 2 - this.c)).toFixed(3);;
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch  {
+
+        return {
+            getArea() {
+              return  "Ошибка! Треугольник не существует";
+            },
+            getPerimeter() {
+               return "Ошибка! Треугольник не существует";
+            }
+        };
+    }
+}
