7df445aefbf4d56be9bab0df0dc1bd74993be1e7
Denis Nikitin <dinya.nikitin@gmail.com>
2021-01-25 08:04:39 +0300

    3.1 change

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -1,8 +1,8 @@
 function parseCount(number) {
     let newNumber = Number.parseInt(number);
     if (isNaN(newNumber)) {
-        const e = new Error("Невалидное значение");
-        throw e;
+
+        throw new Error("Невалидное значение");
     }
     return newNumber;
 }
@@ -21,31 +21,28 @@ class Triangle {
         this.b = b;
         this.c = c;
         if ((a + b) < c || (a + c) < b || (c + b) < a) {
-            const e = new Error("Треугольник с такими сторонами не существует");
-            throw e;
+
+            throw new Error("Треугольник с такими сторонами не существует");
         }
     }
     getPerimeter() {
-        return this.a + this.b + this.c;;
+        return +(this.a + this.b + this.c);
     }
     getArea() {
         const p = this.getPerimeter();
-        return Math.sqrt((p / 2) * (p / 2 - this.a) * (p / 2 - this.b) * (p / 2 - this.c)).toFixed(3);;
+        return +Math.sqrt((p / 2) * (p / 2 - this.a) * (p / 2 - this.b) * (p / 2 - this.c)).toFixed(3);
     }
 }
 
 function getTriangle(a, b, c) {
     try {
         return new Triangle(a, b, c);
-    } catch  {
+    } catch {
 
         return {
-            getArea() {
-              return  "Ошибка! Треугольник не существует";
-            },
-            getPerimeter() {
-               return "Ошибка! Треугольник не существует";
-            }
-        };
+            getArea: () => `Ошибка! Треугольник не существует`,
+            getPerimeter: () => `Ошибка! Треугольник не существует`
+        }
     }
+}
 }
---
+++ 3.2-array-proto-hocs/functions.js
@@ -0,0 +1,5 @@
+console.clear();
+const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
+function getNames() {
+  return this.find(function = () => this.na);
+}
